cc0de573949709454b594133735f3bea1f4e481f
ShannonMochizuki <shannonmochizuki@gmail.com>
2026-09-06 15:53:06 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'joshua-v4';
+const CACHE = 'joshua-v5';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
